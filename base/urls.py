84465f9b09fab41c79e796ad5241b575bcb05983
Anastasiia Bevziuk <anastasiiabevziuk@gmail.com>
2024-05-28 18:13:13 +0300

    add templates for users app

--- base/urls.py
+++ base/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('user/', include('users.urls'))
 ]
