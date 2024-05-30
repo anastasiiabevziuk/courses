80c9f33dd22e619ee39af0e7e9f59ac3913d50aa
Anastasiia Bevziuk <anastasiiabevziuk@gmail.com>
2024-05-30 18:48:04 +0300

    upd nav, added login logic

--- users/views.py
+++ users/views.py
@@ -1,10 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm
 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("profile"))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - LogIn' 
+        'title': 'Home - LogIn' ,
+        'form': form
     }
     return render(request, "users/login.html", context)
 
